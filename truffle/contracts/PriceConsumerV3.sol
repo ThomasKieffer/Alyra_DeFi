fa22da76ea6066aec15acd238a647f593769f591
juliench82 <julienc82@protonmail.com>
2022-07-28 18:09:53 +0200

    update, à vérifier par @TK @GV please!

--- truffle/contracts/PriceConsumerV3.sol
+++ truffle/contracts/PriceConsumerV3.sol
@@ -3,13 +3,36 @@ pragma solidity 0.8.14;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract PriceConsumerV3 {
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice(address priceFeedAddress) public view returns (int) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
-        (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
-        return price;
+contract ChainlinkPriceOracle {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        // DAI / USD
+        priceFeed = AggregatorV3Interface(//address DAI KOVAN);
     }
+
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        // for ETH / USD price is scaled up by 10 ** 8
+        return price / 1e8;
+    }
+}
+
+interface AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int answer,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        );
 }
