5be417a89e79ad18e0f6372679cc0bcdfbb77fff
juliench82 <julienc82@protonmail.com>
2022-07-28 15:35:53 +0200

    PriceConsumerV3 added by @JC

---
+++ truffle/contracts/PriceConsumerV3.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice(address priceFeedAddress) public view returns (int) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+        (uint80 roundID, int price, uint startedAt, uint timeStamp, uint80 answeredInRound) = priceFeed.latestRoundData();
+        return price;
+    }
+}
