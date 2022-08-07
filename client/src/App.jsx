ce01697d4ff70b8b4623c29fe7bc19d661f293e6
Thomas Kieffer <thomaskieffer01@gmail.com>
2022-08-08 00:51:13 +0200

    [UPD] addtoken and tokens

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,13 +39,19 @@ function App() {
         console.log(owner);
 
         // await instance.methods.addToken(rewardTokenAddr, BigInt(1 * 10 ** 18), "DAI").send({ from: accounts[0] });
-        const rewardTokenPrice = await instance.methods.priceOf(rewardTokenAddr).call({ from: accounts[0] });
-        console.log("rewardTokenPrice : ");
-        console.log(rewardTokenPrice);
+        console.log("here : ");
+        await instance.methods.addToken("0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD", "10000000000000000000", "ETH").call({ from: accounts[0] });
 
-        // const token0 = await instance.methods.tokens(0).call({ from: accounts[0] });
-        // console.log("token0 : ");
-        // console.log(token0);
+        console.log("here2 : ");
+        const token0 = await instance.methods.tokens(0).call({ from: accounts[0] });
+        console.log("token0 : ");
+        console.log(token0);
+
+        // console.log("here3 : ");
+        // const rewardTokenPrice = await instance.methods.priceOf(token0).call({ from: accounts[0] });
+        // console.log("rewardTokenPrice : ");
+        // console.log(rewardTokenPrice);
+        // console.log("here4 : ");
       } catch (error) {
         alert(`Failed to load web3, accounts, or contract. Check console for details.`);
         console.error(error);
